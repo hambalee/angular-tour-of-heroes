9d5c156981d68d67bbb14b92d7dfab6ae28952c0
hambalee <hambalee@outlook.com>
2023-02-08 16:36:32 +0700

    4. Add services

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -4,6 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessageService {
+  message: string[] = []
 
   constructor() { }
+
+  add(message: string) {
+    this.message.push(message)
+  }
+
+  clear() {
+    this.message = []
+  }
 }
